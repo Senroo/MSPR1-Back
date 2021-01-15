311c7620fbacd1eb92d5fa258e853ebfdc4df71e
Romain <sengerromain75@gmail.com>
2021-01-15 15:02:34 +0100

    blog

--- back-end/src/Controller/BlogController.php
+++ back-end/src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
@@ -13,16 +14,22 @@ class BlogController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
     /**
      * @Route("/", name="home")
      */
-    public function home(){
-        return $this->render('blog/home.html.twig',[
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
             'age' => 22,
             'title' => 'Symfony officiel de la BAC',
         ]);
@@ -31,7 +38,8 @@ class BlogController extends AbstractController
     /**
      * @Route("blog/article/12", name="blog_show")
      */
-    public function show(){
-        return $this->render('blog/show.html.twig',);
+    public function show()
+    {
+        return $this->render('blog/show.html.twig');
     }
 }
