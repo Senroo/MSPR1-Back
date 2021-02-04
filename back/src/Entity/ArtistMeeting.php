991ce5be79150fbef21f854db9e5b5d443c84fc5
Romain <sengerromain75@gmail.com>
2021-02-04 16:59:25 +0100

    api artist/concert

--- back/src/Entity/ArtistMeeting.php
+++ back/src/Entity/ArtistMeeting.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\ArtistMeetingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=ArtistMeetingRepository::class)
  */
 class ArtistMeeting
